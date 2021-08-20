7be7470e7c7d9e4ef5caeaa7dd1178937f5c38e8
Dalibor989 <lazic_dalibor@hotmail.com>
2021-08-20 12:11:43 +0200

    zadatak 3

--- src/components/AppCustomers.js
+++ src/components/AppCustomers.js
@@ -2,23 +2,72 @@ import React, {useState} from "react";
 
 function AppCustomers() {
 
+  const [newCustomer, setNewCustomer] = useState({
+    index: '',
+    name: '',
+    surname: '',
+    age: '',
+  })
+
   const [customers, setCustomer] = useState([
-    {id: 0, name: 'Pero', surname: 'Peterson', age: 20},
-    {id: 1, name: 'Milan', surname: 'Milanovic', age: 35},
-    {id: 2, name: 'Jovana', surname: 'Jovanovic', age: 28},
+    {index: 0, name: 'Pero', surname: 'Peterson', age: 20},
+    {index: 1, name: 'Milan', surname: 'Milanovic', age: 35},
+    {index: 2, name: 'Jovana', surname: 'Jovanovic', age: 28},
   ])
 
-  const remove = (customerId) => {
-    setCustomer(customers.filter((animals, id) => id !== customerId))
+  const removeCustomer = (customerIndex) => {
+    setCustomer(customers.filter((animals, index) => index !== customerIndex))
+  }
+
+  const addCustomer = (e) => {
+    e.preventDefault();
+
+    setCustomer([...customers, newCustomer]);
+    setNewCustomer({
+      name: '',
+      surname: '',
+      age: '',
+    })
+  }
+
+  const handleNameChange = (e) => {
+    setNewCustomer({
+      ...newCustomer,
+      name: e.target.value
+    })
+  }
+
+  const handleSurnameChange = (e) => {
+    setNewCustomer({
+      ...newCustomer,
+      surname: e.target.value
+    })
+  }
+
+  const handleAgeChange = (e) => {
+    setNewCustomer({
+      ...newCustomer,
+      age: e.target.value
+    })
   }
 
   return (
     <div>
       <ul>
-        {customers.map((customer, id) => (
-          <li key={id}>{customer.name} {customer.surname}<button onClick={() => remove(id)}>Remove Customer</button></li>
+        {customers.map((customer, index) => (
+          <li key={index}>{customer.name} {customer.surname}<button onClick={() => removeCustomer(index)}>Remove Customer</button></li>
         ))}
       </ul>
+      
+      <div>
+        <form onSubmit={addCustomer}>
+          <input required type="text" value={newCustomer.name} placeholder="Name" onChange={handleNameChange}/>
+          <input required type="text" value={newCustomer.surname} placeholder="Surname" onChange={handleSurnameChange}/>
+          <input required type="number" value={newCustomer.age} placeholder="Age" onChange={handleAgeChange}/>
+          <button>Add</button>
+        </form>
+      </div>
+      
     </div>
   )
 }
